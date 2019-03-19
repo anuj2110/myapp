05083c5deb8b60fbbe354733cfe35a951c7b47c2
anuj2110 <atrehan789@gmail.com>
2019-03-20 01:12:03 +0530

    Completed App

--- lib/main.dart
+++ lib/main.dart
@@ -63,10 +63,44 @@ class RandomWordsState extends State<RandomWords> {
       },
     );
   }
+  void _pushSaved() {
+  Navigator.of(context).push(
+    new MaterialPageRoute<void>(
+      builder: (BuildContext context) {
+        final Iterable<ListTile> tiles = _saved.map(
+          (WordPair pair) {
+            return new ListTile(
+              title: new Text(
+                pair.asPascalCase,
+                style: _biggerFont,
+              ),
+            );
+          },
+        );
+        final List<Widget> divided = ListTile
+          .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+              .toList();
+
+        return new Scaffold(         // Add 6 lines from here...
+          appBar: new AppBar(
+            title: const Text('Saved Suggestions'),
+          ),
+          body: new ListView(children: divided),
+        );                           // ... to here.
+      },
+    ),
+  );
+}
   Widget build(BuildContext context){
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Startup Name Generator'),
+        actions: <Widget>[
+          new IconButton(icon:const Icon(Icons.list),onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
